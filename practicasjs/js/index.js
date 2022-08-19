8d9cbdf0e261f09676d1e92d885aa107332f812b
bsquintana55 <bsquintana55@misena.edu.co>
2022-08-19 17:20:09 -0500

    1 de js

---
+++ practicasjs/js/index.js
@@ -0,0 +1,25 @@
+     
+      let gFa=prompt('Ingrese los grados Fahrenheit: ');
+      gFa=parseInt(gFa)
+      let  gCent=(gFa-32)/1.8
+      document.write('Los '+gFa+' grados FahrenHeit es igual a: '+gCent+' grados Centigrados')
+      alert(`El resultado es ${gCent} `)
+    
+
+      
+
+/*     
+      var  gFa;
+      var  gCent;
+
+          gFa=console.log('Ingrese los grados Fahrenheit: ');
+    
+          gCent= (gFar-32)/1.8
+
+    
+            console.log('Los "+gFar+" grados FahrenHeith es igual a: "+gCent+" grados Centigrados')
+    
+
+    */
+
+    
