ea0cb581551a8723b3eb4fb644300be939634a58
bsquintana55 <bsquintana55@misena.edu.co>
2022-08-21 09:31:16 -0500

    retos 19-08

--- js/index.js
+++ js/index.js
@@ -1,25 +1,50 @@
      
-      let gFa=prompt('Ingrese los grados Fahrenheit: ');
+   
+   
+     /*  //   1 RETO TEMPERATURA
+     let gFa=prompt('Ingrese los grados Fahrenheit: ');
       gFa=parseInt(gFa)
       let  gCent=(gFa-32)/1.8
       document.write('Los '+gFa+' grados FahrenHeit es igual a: '+gCent+' grados Centigrados')
       alert(`El resultado es ${gCent} `)
-    
+    */
 
       
 
-/*     
-      var  gFa;
-      var  gCent;
+      /* //RETO 2 ALCANCIA
+     let dine=prompt('Ingrese el dinero a guardar: ');
+      dine=parseInt(dine)
 
-          gFa=console.log('Ingrese los grados Fahrenheit: ');
-    
-          gCent= (gFar-32)/1.8
-
-    
-            console.log('Los "+gFar+" grados FahrenHeith es igual a: "+gCent+" grados Centigrados')
-    
-
+      alert(`Tienes ${dine} guardados en tu alcancia`)
     */
 
-    
+
+      /* // 3 RETO DATOS PERSONALES
+      let nombre=prompt('Ingrese su nombre')
+      let apell=prompt('Ingrese su apellido')
+      let edad=prompt('Ingrese su edad')
+      edad=parseInt(edad)
+      let tel=prompt('Ingrese su telefono')
+      tel=parseInt(tel)
+
+      alert(`Hola, tú nombre es ${nombre} y tu apellido es ${apell}, tienes ${edad} años y tu telefono es ${tel}`)
+
+*/
+     
+   /*   //4 RETO MAYORIA DE EDAD
+
+      let edad=prompt('Ingrese su edad')
+     edad=parseInt(edad)
+
+      if (edad>=18) {
+            alert(`Bienvenido al bar`)
+
+      }else if (edad > 5 && edad < 18) {
+            alert(`Bebe de ${edad}, vaya pa' la casita a ver pocoyo`)
+
+      }else{
+            alert(`Digite una edad real`)
+
+      }
+
+*/
